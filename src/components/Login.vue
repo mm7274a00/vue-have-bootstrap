285fb9ad39219ae7857b64853f035e9456179306
。 <mm7274a00@gmail.com>
2023-10-17 17:49:04 +0800

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,25 @@
+<!--https://www.figma.com/file/TQYQgDnAAKgKl8lbB8FdPE/%E5%89%8D%E7%AB%AF%E7%B7%B4%E7%BF%925---%E6%88%BF%E5%9C%B0%E7%94%A2%E7%AE%A1%E7%90%86%E7%B3%BB%E7%B5%B1?type=design&node-id=106-16186&mode=design&t=lirE3s3OR7gJefDy-0-->
+
 <script>
 import HouseHeader from "../components/HouseHeader.vue";
 export default {
-        data(){
-            return{
-            }
-        },
-        components:{
-            HouseHeader
+    data(){
+        return{
+        }
+    },
+    components: {
+        HouseHeader
+    },
+    methods:{
+        HouseHeader(){
+            console.log()
         }
     }
+}
 </script>
 
 <template>
-<h1>Hi! This is Home</h1>
+<h1>Hi!</h1>
 </template>
 
 <style lang="scss" scoped>

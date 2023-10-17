162e6c7d137d10538221479063e37eeb6f0269d6
。 <mm7274a00@gmail.com>
2023-10-17 14:40:20 +0800

    house

--- src/views/House.vue
+++ src/views/House.vue
@@ -3,10 +3,174 @@
 </script>
 
 <template>
-<h1>Hi! This is Home</h1>
+<div class="houseFull">
+    <div class="header page">
+        <div class="color box"></div>
+
+        <div class="com box">
+            <input type="button" value="会社案内" class="button">
+            <label for="">｜</label>
+            <input type="button" value="店舖、アクセス">
+        
+        </div>
+
+        <div class="phone box">
+            <i class="fa-solid fa-phone"></i>
+            <span><p>010-123456789</p></span>
+        </div>
+
+        <div class="login box">
+            <input type="button" value="ログイン">
+            <label for="">｜</label>
+            <input type="button" value="お問い合わせ">
+        </div>
+    </div>
+
+    <div class="middle page">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="..." class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+    </div>
+
+    <div class="footer page">
+        <div class="under">
+        <a href="https://www.google.com.tw/"></a>
+        <input type="button" value="タッグから探す">
+        <a href="https://www.google.com.tw/"></a>
+        <input type="button" value="エリアから探す">
+        <a href="https://www.google.com.tw/"></a>
+        <input type="button" value="沿線、駅から探す">
+    </div>
+    </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
+$maincolor:#475467;
+.houseFull{
+    width: 1440px;
+    height: 1022px;
+    background-color: white;
+    border: 5px solid $maincolor;
 
+}
+
+.page{
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    height: 50%;
+    /*border: 3px solid black;*/
+}
+
+.header{
+    background-color: white;
+    height: 168px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    
+.box{
+    
+    /*border: 2px solid blue;*/
+    margin: 0 auto;
+}
+    .color{
+        width: 98px;
+        height: 98px;
+        background-color: $maincolor;
+    }
+    .com{
+        width: 300px;
+        height: 24px;
+        display: flex;
+        justify-content: space-between;
+        input{
+            border: 0;
+            background-color: white;
+            width: 40%;
+            height: 100%;
+        }
+    }
+    .phone{
+        color: black;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-weight: 100;
+        font-size: 16pt;
+        width: 204px;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+
+
+    }
+    .login{
+        border-radius: 10px;
+        width: 248px;
+        height: 48px;
+        display: flex;
+        align-items: center;
+        background-color: $maincolor;
+        color: white;
+        input{
+            margin: 0 10px;
+            color: white;
+            background-color: $maincolor;
+            font-weight: bold;
+            border: 0;
+    }
+}
+}
+
+.middle{
+    height: 481px;
+}
+
+.footer{
+    height: 373px;
+    background-color: #BEC0CD;
+
+    .under{
+        width: 100%;
+        margin-top: 58px;
+        display: flex;
+        justify-content: space-around;
+        
+    }
+    input{
+        height: 161px;
+        width: 278px;
+        font-size: 16pt;
+        border: 0;
+        border-radius: 15px;
+        background-color: $maincolor;
+        color: white;
+    }
+}
 
 </style>
